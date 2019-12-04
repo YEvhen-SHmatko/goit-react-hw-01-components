17276db7f824780b8fb61cbf084449845e5807bf
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2019-12-04 13:11:02 +0200

    Updates

---
+++ precache-manifest.48b98c2706d5191661facf6258c8d727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1bdafed3dfbce091faedc16da8d491",
+    "url": "/YEvhen-SHmatko/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f559cbbe42e1e721f752",
+    "url": "/YEvhen-SHmatko/goit-react-hw-01-components/static/css/main.fbb8aeba.chunk.css"
+  },
+  {
+    "revision": "e5276d3a4c2318d0e826",
+    "url": "/YEvhen-SHmatko/goit-react-hw-01-components/static/js/2.61c7b585.chunk.js"
+  },
+  {
+    "revision": "f559cbbe42e1e721f752",
+    "url": "/YEvhen-SHmatko/goit-react-hw-01-components/static/js/main.629ff3de.chunk.js"
+  },
+  {
+    "revision": "22f3a9c6a6a75cddadff",
+    "url": "/YEvhen-SHmatko/goit-react-hw-01-components/static/js/runtime-main.9d28c8ba.js"
+  }
+]);
