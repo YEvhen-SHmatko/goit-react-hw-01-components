7f1f9f25d3c8f7b65b3581cf30921c18b287348f
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2019-12-08 11:10:17 +0200

    edit clean code

--- src/App.js
+++ src/App.js
@@ -8,25 +8,13 @@ import dataFriendList from './components/FriendList/friends.json';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import dataTransactionHistory from './components/TransactionHistory/transactions.json';
 
-const appStyles = {
-  width: '100%',
-  minHeight: '50vh',
-  padding: '40px 10px',
-  background: 'rgba(231, 236, 242)',
-  display: 'flex',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  alignItems: 'center',
-};
 const App = () => (
-  <div>
-    <div style={{ ...appStyles }}>
-      <Profile user={userProfile} />
-      <Statistics title="File upload" stats={dataStatistics} />
-      <FriendList friends={dataFriendList} />
-      <TransactionHistory item={dataTransactionHistory} />
-    </div>
-  </div>
+  <>
+    <Profile user={userProfile} />
+    <Statistics title="File upload" stats={dataStatistics} />
+    <FriendList friends={dataFriendList} />
+    <TransactionHistory item={dataTransactionHistory} />
+  </>
 );
 
 export default App;
